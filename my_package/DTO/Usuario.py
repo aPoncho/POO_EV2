7f72e284a99fc0991bff10d86c4e38aec28f336c
aPoncho <fbarrioscor@gmail.com>
2024-11-25 16:24:50 -0300

    correccion usuario

--- my_package/DTO/Usuario.py
+++ my_package/DTO/Usuario.py
@@ -1,14 +1,15 @@
 from my_package.DTO.Persona import Persona
 
 class Usuario(Persona):
-    def __init__(self, run, nombre, apellido, direccion, fono, correo, id, password):
+    def __init__(self, run, nombre, apellido, direccion, fono, correo, username, password, tipo_usuario):
         super().__init__(run, nombre, apellido, direccion, fono, correo)
         #atributos deberian privados (no se puede acceder a ellos fuera de la clase)
-        self.id = id
-        self.pasword = password
+        self.username = username
+        self.pasword_hash = password
+        self.tipo_usuario = tipo_usuario
 
     def __str__(self):
         datos = super().__str__()
-        return f"Usuario: {self.id} \n {datos}"
+        return f"Usuario: {self.username} \n {datos}"
 
 
