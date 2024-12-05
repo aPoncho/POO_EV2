075b193f22a213a366edf8e7d09886215359df70
aPoncho <fbarrioscor@gmail.com>
2024-12-05 19:18:22 -0300

    ver 3.0

--- my_package/DTO/Proyecto.py
+++ my_package/DTO/Proyecto.py
@@ -4,3 +4,6 @@ class Proyecto():
         self.descripcion_proyecto = descripcion
         self.fecha_inicio_proyecto = fecha
         self.id_depto = id_depto
+
+    def __str__(self):
+        return f"NOMBRE PROYECTO: {self.nombre_proyecto}\nDESCRIPCION: {self.descripcion_proyecto}\nINICIO: {self.fecha_inicio_proyecto}\n DEPTO ID: {self}"
